45400dd0cbc4df9cee177f79a6cb0c4fc7dacc93
Personal Mubeen <mt.mubeentahir@gmail.com>
2019-06-11 03:18:47 +0500

    Added spec for stats action

--- spec/requests/readings_controller_spec.rb
+++ spec/requests/readings_controller_spec.rb
@@ -20,6 +20,7 @@ RSpec.describe Api::V1::ReadingsController do
       data = JSON.parse(response.body)
       expect(data.count).to eq(3)
     end
+
   end
 
   describe 'POST #readings' do
@@ -38,11 +39,45 @@ RSpec.describe Api::V1::ReadingsController do
     end
 
     it "JSON body response contains expected attributes" do
-      byebug
       data = JSON.parse(response.body)
       expect(data.keys.map(&:to_sym)).to match_array([:temprature, :humidity, :battery_charge, :sequence])
     end
   end
 
+  describe 'GET #stats' do
+    before do
+      get "/api/v1/stats/#{thermostat.household_token}"
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it "JSON body response contains expected attributes" do
+      data = JSON.parse(response.body)
+      expect(data.keys.map(&:to_sym)).to match_array([:id, :household_token, :location, :minimum_temprature, :average_temprature, :maximum_temprature, :minimum_humidity, :average_humidity, :maximum_humidity, :minimum_battery_charge, :average_battery_charge, :maximum_battery_charge])
+    end
+
+    it 'Response contains expected temprature' do
+      data = JSON.parse(response.body)
+      expect(data['minimum_temprature']).to eq(thermostat.readings.minimum(:temprature))
+      expect(data['average_temprature']).to eq(thermostat.readings.average(:temprature).to_i)
+      expect(data['maximum_temprature']).to eq(thermostat.readings.maximum(:temprature))
+    end
+
+    it 'Response contains expected humidity' do
+      data = JSON.parse(response.body)
+      expect(data['minimum_humidity']).to eq(thermostat.readings.minimum(:humidity))
+      expect(data['average_humidity']).to eq(thermostat.readings.average(:humidity).to_i)
+      expect(data['maximum_humidity']).to eq(thermostat.readings.maximum(:humidity))
+    end
+
+    it 'Response contains expected battery_charge' do
+      data = JSON.parse(response.body)
+      expect(data['minimum_battery_charge']).to eq(thermostat.readings.minimum(:battery_charge))
+      expect(data['average_battery_charge']).to eq(thermostat.readings.average(:battery_charge).to_i)
+      expect(data['maximum_battery_charge']).to eq(thermostat.readings.maximum(:battery_charge))
+    end
+  end
 
 end
